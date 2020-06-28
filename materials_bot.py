00d616250790ce1dc531a02a6698f9f87b6d8464
neculac757 <neculac757@gmail.com>
2020-06-28 13:11:00 +0500

    new code1

--- materials_bot.py
+++ materials_bot.py
@@ -44,9 +44,11 @@ def makeWebhookResult(req):
         }
 
 if __name__=='__main__':
-    port= int(os.getenv('PORT',5000))
-    print("starting server on port",port)
-    app.run(debug=True,port=port,host='0.0.0.0')
+    app.debug=False
+    app.run()
+#     port= int(os.getenv('PORT',5000))
+#     print("starting server on port",port)
+#     app.run(debug=True,port=port,host='0.0.0.0')
    
     
     
